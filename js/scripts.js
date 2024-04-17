275443ff05741fc39f7689802143a438f29a2253
jeansonmylegs <122405167+jeansonmylegs@users.noreply.github.com>
2024-04-16 22:49:44 -0500

    WORKING ROUGH DRAFT , REWROTE JAVASCRIPT FILE

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,39 @@ const mobileMenuClose = document.querySelector('#mobile_menu_close');
 const mobileMenu = document.querySelector('#mobile_menu');
 
 
+function openMobileMenu(){
+    mobileMenu.classList.remove('translate-x-full');
+    mobileMenu.classList.add('translate-x-0');
+}
+
+function closeMobileMenu() {
+    // Add the class to hide the menu and remove the class to show it
+    mobileMenu.classList.add('translate-x-full');
+    mobileMenu.classList.remove('translate-x-0');
+}
+
+// Adding event listener for opening the mobile menu
+mobileMenuTrigger.addEventListener('click', openMobileMenu);
+
+// Adding event listener for closing the mobile menu
+mobileMenuClose.addEventListener('click', closeMobileMenu);
+
+
 
 
 
 //  Open mobile menu when the menu button is clicked, 
 // eventhandler that listens for a click and executes function
-mobileMenuTrigger.addEventListener('click', function () {
-    mobileMenu.classList.remove('translate-x-full');
-    mobileMenu.classList.add('translate-x-0');
-});
+//mobileMenuTrigger.addEventListener('click', function () {
+  //  mobileMenu.classList.remove('translate-x-full');
+    //mobileMenu.classList.add('translate-x-0');
+//});
 
 
 
 
 //  Close mobile menu when the menu button is clicked
-mobileMenuClose.addEventListener('click', function () {
-    mobileMenu.classList.add('translate-x-full');
-    mobileMenu.classList.remove('translate-x-0');
-});
+//mobileMenuClose.addEventListener('click', function () {
+    //mobileMenu.classList.add('translate-x-full');
+    //mobileMenu.classList.remove('translate-x-0');
+//});
